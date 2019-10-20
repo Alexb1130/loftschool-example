62cab915b7af69ef02e6f10db9386d8023f58682
alx <alx@alxs-MacBook-Pro.local>
2019-10-20 22:16:53 +0300

    dnd

--- src/dnd.js
+++ src/dnd.js
@@ -17,6 +17,16 @@
  */
 const homeworkContainer = document.querySelector('#homework-container');
 
+// function getRandomInt (min, max) {
+//     return parseInt(min + Math.random() * max - min);
+// }
+
+function getRandomInt (min, max) {
+    const length = max - min + 1;
+
+    return parseInt(Math.random() * length) + min;
+}
+
 /*
  Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
  Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
@@ -28,8 +38,17 @@ const homeworkContainer = document.querySelector('#homework-container');
  */
 function createDiv() {
     const div = document.createElement('div');
+    const minSize = 20;
+    const maxSize = 200;
+    const maxColor = 0xffffff;
 
     div.classList.add('draggable-div');
+    div.style.position = 'absolute';
+    div.style.top = getRandomInt(0, window.innerHeight) + 'px';
+    div.style.left = getRandomInt(0, window.innerWidth) + 'px';
+    div.style.width = getRandomInt(minSize, maxSize) + 'px';
+    div.style.height = getRandomInt(minSize, maxSize) + 'px';
+    div.style.backgroundColor = '#' + getRandomInt(0, maxColor).toString(16);
 
     return div;
 }
@@ -43,11 +62,48 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+    let startCoords = {};
+
+    target.addEventListener('mousedown', e => {
+        e.preventDefault();
+
+        startCoords = {
+            x: e.clientX,
+            y: e.clientY    
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    })
+
+    const onMouseMove = moveE => {
+        moveE.preventDefault();
+
+        const shift = {
+            x: startCoords.x - moveE.clientX,
+            y: startCoords.y - moveE.clientY
+        }
+        
+        startCoords = {
+            x: moveE.clientX,
+            y: moveE.clientY
+        }
+
+        target.style.top = target.offsetTop - shift.y + 'px';
+        target.style.left = target.offsetLeft - shift.x + 'px';
+    }
+
+    const onMouseUp = upE => {
+        upE.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function() {
+addDivButton.addEventListener('click', function () {
     // создать новый div
     const div = createDiv();
 
